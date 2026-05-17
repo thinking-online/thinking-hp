f0e86b4f5f424892ee70495de80189925eea61df
thinking-llcarc <llcarc1110@gmail.com>
2026-05-17 15:42:28 +0900

    LINEリンク変更

--- data/line-url.js
+++ data/line-url.js
@@ -2,4 +2,4 @@
  * LINE 公式導線（LIFF ランディング）。各ページで React より前に読み込む。
  */
 window.THINKING_LINE_LIFF_URL =
-  "https://liff.line.me/1656043253-rkMxPZMQ/landing?follow=%40499yrupi&lp=MFzZMH&liff_id=1656043253-rkMxPZMQ";
+  "https://liff.line.me/1656043253-rkMxPZMQ/landing?follow=%40499yrupi&lp=0ah1RL&liff_id=1656043253-rkMxPZMQ";
